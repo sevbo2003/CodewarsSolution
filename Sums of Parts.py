cd547d90c8fb56674d7cc69e8f951ee1475d9322
justSevbo <Github2003@123>
2021-12-11 11:49:01 +0500

    6 KYU solved

--- Sums of Parts.py
+++ Sums of Parts.py
@@ -1,18 +1,10 @@
 """https://www.codewars.com/kata/5ce399e0047a45001c853c2b/train/python"""
 
-from functools import reduce
-
-
 def parts_sums(ls):
-    if len(ls) == 0:
-        return [0]
-    else:
-        totalSum = reduce(lambda a, b: a + b, ls)
-        sumList = [totalSum]
-        for i in range(1, len(ls) + 1):
-            totalSum -= ls[i - 1]
-            sumList.append(totalSum)
-        return sumList
+    result = [sum(ls)]
+    for item in ls:
+        result.append(result[-1]-item)
+    return result
 
 
 print(parts_sums([0, 1, 3, 6, 10]))
